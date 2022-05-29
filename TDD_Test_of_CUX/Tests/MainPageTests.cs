010089110be296098ce3bdecccc1ec447543dfc2
mdarda <maciejdarda@gmail.com>
2022-05-29 19:36:32 +0200

    Inprove nlog.config

--- TDD_Test_of_CUX/Tests/MainPageTests.cs
+++ TDD_Test_of_CUX/Tests/MainPageTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using NLog;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using TDD_Test_of_CUX.Pages;
 
@@ -8,6 +9,8 @@ namespace TDD_Test_of_CUX.Tests
     [Category("Main Page funcionality and navigation")]
     internal class MainPageTests : BaseTest
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
         public IWebElement logo => Driver.FindElement(By.XPath(@"//img[@class='logo img-responsive']"));
         public IWebElement popularTab => Driver.FindElement(By.XPath(@"//a[@href='#homefeatured']"));
         public IWebElement bestSellersTab => Driver.FindElement(By.XPath(@"//a[@href='#blockbestsellers']"));
@@ -22,16 +25,18 @@ namespace TDD_Test_of_CUX.Tests
             Assert.IsTrue(Driver.Title == "My Store" && logo.Displayed);
         }
 
-        [Description("Test swiching between POPULAR and BEST SELLERS tabs")]
+        [Description("Test switching between POPULAR and BESTSELLERS tabs")]
         [Property("Author", "Maciej Darda")]
         [Test]
         public void TCID2()
         {
+            _logger.Debug("TCID2 - started");
             var mp = new MainPage(Driver);
             mp.popularAndBestSellersTab.SwitchToBestSellersTab();
             Assert.IsTrue(bestSellersTabParent.GetAttribute("class") == "active");
             mp.popularAndBestSellersTab.SwitchToPopularTab();
             Assert.IsTrue(popularTabParent.GetAttribute("class") == "active");
+            _logger.Debug("TCID2 - stopped");
         }
     }
 }
