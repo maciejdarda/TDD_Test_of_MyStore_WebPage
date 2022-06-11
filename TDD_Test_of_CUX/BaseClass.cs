d045909200aed9892bb30d45142aaa5d68541e44
mdarda <maciejdarda@gmail.com>
2022-06-11 19:47:52 +0200

    Add MoveToElement method in BaseClass

--- TDD_Test_of_CUX/BaseClass.cs
+++ TDD_Test_of_CUX/BaseClass.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Threading;
 
@@ -16,11 +17,20 @@ namespace TDD_Test_of_CUX
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
+        //accessible in all derived classes
         public void HighlightElement(IWebElement elementToHL)
         {
             var jsDriver = (IJavaScriptExecutor)_driver;
             string highlightJavascript = @"$(arguments[0]).css({ ""border-width"" : ""2px"", ""border-style"" : ""solid"", ""border-color"" : ""red"" });";
             jsDriver.ExecuteScript(highlightJavascript, new object[] { elementToHL }); ;
+        }
+
+        //accessible in all derived classes
+        public void MoveToElement(IWebElement element)
+        {
+            Actions actions = new Actions(_driver);
+            actions.MoveToElement(element);
+            actions.Perform();
         }
     }
 }
