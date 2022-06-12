d0db9ff6928aea16ec17070b6f73ba7c0c256bbf
mdarda <maciejdarda@gmail.com>
2022-06-12 19:36:16 +0200

    Add Newsletter class

---
+++ TDD_Test_of_CUX/Pages/Newsletter.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using TDD_Test_of_CUX;
+
+namespace TDD_Test_of_MyStore.Pages
+{
+    internal class Newsletter : BaseClass
+    {
+        //constructor with reference to BaseClass
+        public Newsletter(IWebDriver driver) : base(driver)
+        {
+        }
+
+        IWebElement NewsletterElement => _driver.FindElement(By.Id("newsletter_block_left"));
+
+        internal void SendKeysToNewsLetter(string text)
+        {
+            //method from BaseClass
+            MoveToElement(NewsletterElement);   
+
+            //method from BaseClass
+
+        }
+    }
+}
