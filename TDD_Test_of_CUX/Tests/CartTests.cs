0e305bc89ea508e1af750ceaa82dbe125e3af92d
mdarda <maciejdarda@gmail.com>
2022-06-22 22:46:32 +0200

    Add CartTests class

---
+++ TDD_Test_of_CUX/Tests/CartTests.cs
@@ -0,0 +1,13 @@
+﻿using TDD_Test_of_CUX.Tests;
+using NUnit.Framework;
+using NLog;
+
+namespace TDD_Test_of_MyStore.Tests
+{
+    [TestFixture]
+    [Category("Cart funcionality and navigation")]
+    class CartTests : BaseTest
+    {
+        private static Logger LoggerObj = LogManager.GetCurrentClassLogger();
+    }
+}
