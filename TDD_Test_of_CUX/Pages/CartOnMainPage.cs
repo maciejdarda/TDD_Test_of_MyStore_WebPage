3daf81a925ddbaed694186511bc2b22f9c40af89
mdarda <maciejdarda@gmail.com>
2022-06-23 16:40:33 +0200

    Add CartOnMainPage class

---
+++ TDD_Test_of_CUX/Pages/CartOnMainPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace TDD_Test_of_MyStore.Pages
+{
+    class CartOnMainPage : BaseClass
+    {
+        public CartOnMainPage(IWebDriver driver) : base(driver)
+        {         
+        }
+    }
+}
