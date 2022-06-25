859a91e6d7e2e269c7d70f5785887305ef0665eb
mdarda <maciejdarda@gmail.com>
2022-06-25 16:28:03 +0200

    Add CentarColumn class

---
+++ TDD_Test_of_CUX/Pages/CentralColumn.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD_Test_of_MyStore.Pages
+{
+    internal class CentralColumn : BaseClass
+    {
+        public CentralColumn(IWebDriver driver) : base(driver)
+        {
+        }
+
+    }
+}
