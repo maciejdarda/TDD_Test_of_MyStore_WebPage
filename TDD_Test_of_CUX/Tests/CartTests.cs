c95b3faadb7dcb5aafbc1a1e5ca89ddbb1e8a48a
mdarda <maciejdarda@gmail.com>
2022-06-29 23:58:34 +0200

    Change the naming of the test methods in CartTests

--- TDD_Test_of_CUX/Tests/CartTests.cs
+++ TDD_Test_of_CUX/Tests/CartTests.cs
@@ -7,32 +7,22 @@ namespace TDD_Test_of_MyStore.Tests
     [Category("Cart funcionality and navigation")]
     class CartTests : BaseTest
     {
-        private static Logger LoggerObj = LogManager.GetCurrentClassLogger();
-
         [Description("Cart - test opening of the cart page")]
         [Property("Author", "Maciej Darda")]
         [Test]
-        public void TCID1()
+        public void TestCase_1_CartPageShouldOpen()
         {
-            LoggerObj.Debug("TCID1 - started");
-
             PageObejcts.CartOnMainPage.OpenCartPage();
             PageObejcts.CartOnMainPage.AssertThatCartPageIsOpen();
-
-            LoggerObj.Debug("TCID1 - stopped");
         }
 
         [Description("Cart - test add item to the cart")]
         [Property("Author", "Maciej Darda")]
         [Test]
-        public void TCID2()
+        public void TestCase_2_AddItemToCartShouldBePosible()
         {
-            LoggerObj.Debug("TCID2 - started");
-
             PageObejcts.CentralColumn.AddItemToCart();
             PageObejcts.CartOnMainPage.AssertThatItemIsAddedToCart();
-
-            LoggerObj.Debug("TCID2 - stopped");
         }
     }
 }
